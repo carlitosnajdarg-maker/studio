2222c6104f0ad6eb3aa3f84bcc2b2a45ce734bef
The Galáctic Empire <carlitosnajdarg@gmail.com>
2026-02-18 04:12:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8059120230-35888",
+  "appId": "1:984627255660:web:71e9419298cb5a2131fb49",
+  "apiKey": "AIzaSyBR4ikojSjQADbR4L9rauEp3rb8NK1dMWk",
+  "authDomain": "studio-8059120230-35888.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "984627255660"
+};
